32e1c84e11c26878cf9bb1840a4d90ea299be2f4
Masato Fujimori <suneo3476@gmail.com>
2016-11-26 01:32:00 +0900

    一時的にスイッチを無効

--- kyun/src.js
+++ kyun/src.js
@@ -8,7 +8,7 @@ $(document).ready(()=>{
   		 .done(function(res) {
     	res.get('/1.1/account/verify_credentials.json').done(function(data) {
     		// console.log(data)
-   		$('#icon').attr('src',data.profile_image_url)
+	   		$('#icon').attr('src',data.profile_image_url)
     	})
 	})
 })
