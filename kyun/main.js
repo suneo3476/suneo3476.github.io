cd8052f0f614984cd37b7f2955c8968cd0577c12
Masato Fujimori <suneo3476@gmail.com>
2016-11-26 01:20:20 +0900

    twitter 連携してアイコンを出せるように

--- kyun/main.js
+++ kyun/main.js
@@ -1,6 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-$('#textarea-submit').click(function(e) {
-    return e.preventDefault();                      //<=★コレよ。
-});
-$("[name='textarea-switch']").bootstrapSwitch();
+$(document).ready(()=>{
+	$('#textarea-submit').click((e) => {
+	    return e.preventDefault();                      //<=★コレよ。
+	});
+	$("[name='textarea-switch']").bootstrapSwitch();
+	OAuth.initialize('p9ec33SRheUKfTzs-y-6crX_ros');
+	OAuth.popup('twitter')
+  		 .done(function(res) {
+    	res.get('/1.1/account/verify_credentials.json').done(function(data) {
+    		$('#icon').attr('src','http://pbs.twimg.com/profile_images/792720683486687232/O1HpqMR__normal.jpg')
+    	})
+	})
+})
 },{}]},{},[1]);
--- kyun/src.js
+++ kyun/src.js
@@ -1,4 +1,13 @@
-$('#textarea-submit').click(function(e) {
-    return e.preventDefault();                      //<=★コレよ。
-});
-$("[name='textarea-switch']").bootstrapSwitch();
+$(document).ready(()=>{
+	$('#textarea-submit').click((e) => {
+	    return e.preventDefault();                      //<=★コレよ。
+	});
+	$("[name='textarea-switch']").bootstrapSwitch();
+	OAuth.initialize('p9ec33SRheUKfTzs-y-6crX_ros');
+	OAuth.popup('twitter')
+  		 .done(function(res) {
+    	res.get('/1.1/account/verify_credentials.json').done(function(data) {
+    		$('#icon').attr('src','http://pbs.twimg.com/profile_images/792720683486687232/O1HpqMR__normal.jpg')
+    	})
+	})
+})
