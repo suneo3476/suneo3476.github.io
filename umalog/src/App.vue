6ad6a6dee7219c04e6e400478bcd1b72938ac0ef
suneo3476 <suneo3476@gmail.com>
2021-10-12 23:30:52 +0900

    [umalog] スクショを適当にうｐ。
    あとトップページの気になる所を修正。

---
+++ umalog/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="container">
+      乗馬ログ
+      <div>とりあえずツイートのスクショで勘弁してください。</div>
+      <div class="photo" v-for="(photo, i) in photos" :key="i">
+        <img :src="photo" alt="photo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  computed: {
+    photos() {
+      return [...new Array(19)]
+        .map((elm, idx) => {
+          return require(`@/assets/img/tweet-ss${idx + 1}.png`);
+        })
+        .reverse();
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      console.log(this.photos);
+    });
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  background: #161d26;
+  color: white;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
