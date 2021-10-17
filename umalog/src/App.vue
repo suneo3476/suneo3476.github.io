69ec17994ef65a472e7bfc6dc0173211a67c6375
suneo3476 <suneo3476@gmail.com>
2021-10-17 18:13:48 +0900

    [umalog] 一度コミット

--- umalog/src/App.vue
+++ umalog/src/App.vue
@@ -1,51 +1,60 @@
 <template>
-  <div id="app">
-    <div class="container">
-      乗馬ログ
-      <div>とりあえずツイートのスクショで勘弁してください。</div>
-      <div class="photo" v-for="(photo, i) in photos" :key="i">
-        <img :src="photo" alt="photo" />
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
-    </div>
-  </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  name: "App",
-  components: {},
-  computed: {
-    photos() {
-      return [...new Array(19)]
-        .map((elm, idx) => {
-          return require(`@/assets/img/tweet-ss${idx + 1}.png`);
-        })
-        .reverse();
-    },
+  name: 'App',
+
+  components: {
+    HelloWorld,
   },
-  mounted() {
-    this.$nextTick(() => {
-      console.log(this.photos);
-    });
-  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-}
-.container {
-  background: #161d26;
-  color: white;
-  width: 100%;
-  height: 100%;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-</style>
--- umalog/vue.config.js
+++ umalog/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/umalog/'
-    : '/'
+    : '/',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
